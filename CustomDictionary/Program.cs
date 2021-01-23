b07cf0ecf3c8bcb2681674d4a1f0acfd58514991
elif ozturk <elf.oztrk.66@gmail.com>
2021-01-23 20:19:20 +0300

    Add project files.

---
+++ CustomDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                MyDictionary<int, string> myDic = new MyDictionary<int, string>();
+                myDic.Add(1, "Elma");
+                myDic.Add(2,"Armut");
+
+        
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
